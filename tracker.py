b3aa98ee84e67e20cd3fdf348e20df053d06653c
tanishi2-cpu <tanishi.jain2@gmail.com>
2026-05-09 21:22:30 +1000

    Update tracker.py
    Add main menu system

--- tracker.py
+++ tracker.py
@@ -1,1 +1,13 @@
-print("Learning Tracker")
+print("=== Learning Tracker ===")
+
+while True:
+    print("\n1. Add completed task")
+    print("2. View progress")
+    print("3. Exit")
+
+    choice = input("Select an option: ")
+
+    if choice == "3":
+        print("Goodbye!")
+        break
+    
